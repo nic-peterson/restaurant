06a2129566448548709108bb15f68e9175a26c5b
Nic Peterson <nic@peterson.to>
2022-02-13 09:04:13 -0800

    Style nav bar and add cursor:pointer & logo.

--- src/functions/initialLoad.js
+++ src/functions/initialLoad.js
@@ -5,6 +5,9 @@ const initialLoad = function () {
   const content = document.querySelector("#content");
   const navBar = nav();
   content.appendChild(navBar);
+  const logo = document.createElement("div");
+  logo.setAttribute("id", "logo");
+  content.appendChild(logo);
   about();
 };
 
