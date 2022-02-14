a75dad98e670e7f25899d39bae823efb17b94549
Nic Peterson <nic@peterson.to>
2022-02-13 17:30:44 -0800

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
