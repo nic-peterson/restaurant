4bab07c02538f44390ac8955d3d64ab45c7b650c
Nic Peterson <nic@peterson.to>
2022-02-15 19:25:58 -0800

    Refactor and comment menu page.

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -13,41 +13,49 @@ const menu = function () {
   const menuContainer = document.createElement("div");
   menuContainer.setAttribute("class", "menu-container");
 
+  // Create pictures for menu items
   const chickenPic = new Image();
   chickenPic.src = Chicken;
-  const chicken = {
-    name: "CRISPY CHICKEN $15",
-    img: chickenPic.src,
-    desc: "roasted garlic aioli, pickle, cilantro slaw",
-    id: "menu-chicken",
-  };
 
   const americanaPic = new Image();
   americanaPic.src = Americana;
-  const americana = {
-    name: "AMERICANA BURGER $20",
-    img: americanaPic.src,
-    desc: "cheese, sauce, lettuce, pickle, onion",
-    id: "menu-americana",
-  };
+
   const melonPic = new Image();
   melonPic.src = Melon;
-  const salad = {
-    name: "MELON & HERB SALAD $12",
-    img: melonPic.src,
-    desc: "melon, cucumber, queso fresco",
-    id: "menu-salad",
-  };
+
   const ricottaPic = new Image();
   ricottaPic.src = Ricotta;
-  const ricotta = {
-    name: "HOUSE MADE RICOTTA $14",
-    img: ricottaPic.src,
-    desc: "rosemary honey, lavosh",
-    id: "menu-ricotta",
-  };
-  const arr = [chicken, americana, salad, ricotta];
-  arr.forEach((elt) => {
+
+  // array of menu objects
+  const menuArr = [
+    {
+      name: "CRISPY CHICKEN $15",
+      img: chickenPic.src,
+      desc: "roasted garlic aioli, pickle, cilantro slaw",
+      id: "menu-chicken",
+    },
+    {
+      name: "AMERICANA BURGER $20",
+      img: americanaPic.src,
+      desc: "cheese, sauce, lettuce, pickle, onion",
+      id: "menu-americana",
+    },
+    {
+      name: "MELON & HERB SALAD $12",
+      img: melonPic.src,
+      desc: "melon, cucumber, queso fresco",
+      id: "menu-salad",
+    },
+    {
+      name: "HOUSE MADE RICOTTA $14",
+      img: ricottaPic.src,
+      desc: "rosemary honey, lavosh",
+      id: "menu-ricotta",
+    },
+  ];
+
+  // cycle through array and append HTML menu items
+  menuArr.forEach((elt) => {
     const menuItem = createCard(elt);
     menuContainer.appendChild(menuItem);
   });
@@ -57,4 +65,3 @@ const menu = function () {
 };
 
 export default menu;
-
