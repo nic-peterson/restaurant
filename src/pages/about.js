88d5e9f11097506e03ab7f5b211cf829594b0e27
Nic Peterson <nic@peterson.to>
2022-02-15 19:19:23 -0800

    Refactor about page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,39 +3,62 @@ import Adam from "../assets/adam.jpeg";
 import Tom from "../assets/tom.jpeg";
 import "../style.css";
 
-
 const about = function () {
   const content = document.querySelector("#content");
   const about = document.createElement("div");
   about.setAttribute("class", "about");
-  
+
   const adamPic = new Image();
   adamPic.src = Adam;
-
-  const owner1 = {
-    name: "Chef Adam Rosenblum",
-    img: adamPic.src,
-    desc: `Chef Adam opened Causwells in 2014`,
-    id: "owner-adam"
-  }
-  
-  const owner1Card = createCard(owner1);
 
   const tomPic = new Image();
   tomPic.src = Tom;
 
+  const owners = [
+    {
+      name: "Chef Adam Rosenblum",
+      img: adamPic.src,
+      desc: `Chef Adam opened Causwells in 2014`,
+      id: "owner-adam",
+    },
+    {
+      name: "Tom Patella",
+      img: tomPic.src,
+      desc: `Tom is a former art teacher`,
+      id: "owner-tom",
+    },
+  ];
+
+  /*
+  const owner1 = {
+    name: "Chef Adam Rosenblum",
+    img: adamPic.src,
+    desc: `Chef Adam opened Causwells in 2014`,
+    id: "owner-adam",
+  };
+  */
+
+  owners.forEach(elt => {
+    const owner = createCard(elt);
+    about.appendChild(owner);
+  })
+  
+  //const owner1Card = createCard(owner);
+
+  
+  /*
   const owner2 = {
     name: "Tom Patella",
     img: tomPic.src,
     desc: `Tom is a former art teacher`,
-    id: "owner-tom"
-  }
+    id: "owner-tom",
+  };
 
   const owner2Card = createCard(owner2);
-  
-  
+
   about.appendChild(owner1Card);
   about.appendChild(owner2Card);
+  */
 
   content.appendChild(about);
 };
